15f8efc54a23b94d3dce4fdee0b208d91ce7dbb9
andreiserban123 <andyspeed2003@gmail.com>
2025-03-07 15:18:04 +0200

    era bine cu 0

--- src/teme/t1/Problema.java
+++ src/teme/t1/Problema.java
@@ -27,7 +27,7 @@ public class Problema {
         }
 
 
-        int countLarger = 1;
+        int countLarger = 0;
         java.util.Set<Integer> valoriUnice = new java.util.HashSet<>();
 
         for (int p : punctaje) {
@@ -38,6 +38,6 @@ public class Problema {
             }
         }
 
-        return countLarger;
+        return countLarger + 1;
     }
 }
