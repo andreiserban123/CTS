f0feb24f0d2445bd42be44621aaa6b4ab92e6e86
andreiserban123 <andyspeed2003@gmail.com>
2025-03-29 18:15:36 +0200

    implement oven management and dish addition functionality

--- src/seminar/week5/singletonRegistry/program/Main.java
+++ src/seminar/week5/singletonRegistry/program/Main.java
@@ -1,6 +1,24 @@
 package seminar.week5.singletonRegistry.program;
 
+import seminar.week5.singletonRegistry.models.Dish;
+import seminar.week5.singletonRegistry.models.Oven;
+
 public class Main {
     public static void main(String[] args) {
+        Oven.createOven(200);
+        Oven.createOven(250);
+        Oven.createOven(300);
+        Oven.createOven(350);
+
+        var list = Oven.getOvensRegistry();
+
+        Oven.addDish(new Dish("Pizza", 30, 300));
+        Oven.addDish(new Dish("Pizza", 30, 300));
+        Oven.addDish(new Dish("Pizza", 180, 300));
+        Oven.addDish(new Dish("Pizza", 20, 300));
+        Oven.addDish(new Dish("Pizza", 20, 300));
+
+        list.forEach(System.out::println);
+
     }
 }
